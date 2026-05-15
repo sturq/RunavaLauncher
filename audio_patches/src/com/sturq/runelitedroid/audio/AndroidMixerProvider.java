5562f0ee653c843cc552a1126e48b113d75c7501
sturq <81992095+sturq@users.noreply.github.com>
2026-05-15 18:19:53 +0200

    Revert "audio: add static-init log to confirm SPI class is being loaded"
    This reverts commit 2b997b13f37fabd64280a3ec42fbcefdd5284036.

--- audio_patches/src/com/sturq/runelitedroid/audio/AndroidMixerProvider.java
+++ audio_patches/src/com/sturq/runelitedroid/audio/AndroidMixerProvider.java
@@ -16,10 +16,6 @@ import javax.sound.sampled.spi.MixerProvider;
  */
 public class AndroidMixerProvider extends MixerProvider {
 
-    static {
-        System.out.println("[rldroid_audio] AndroidMixerProvider loaded — SPI registration successful");
-    }
-
     private static final Mixer.Info INFO = new AndroidMixerInfo();
 
     @Override
